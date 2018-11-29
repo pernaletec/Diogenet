b9257d316eae6b44510cd355bffa3d93e367c316
César Pernalete <pernalete.cg@gmail.com>
2018-11-28 23:21:32 -0400

    Versión Original
    Entrega 20 de Noviembre. Versión Rmarkdown

---
+++ egograph_vnw/app.R
@@ -0,0 +1,324 @@
+
+library(shiny)
+library(rmarkdown)
+library(knitr)
+library(rsconnect)
+library(igraph)
+library(stringi)
+library(tidyverse)
+library(visNetwork)
+
+# Egograph visnetwork
+
+# Import node list: contains nodes$Name, nodes$Group
+nodes <- read.csv('new_Nodes.csv', 
+                  sep=",", 
+                  header = TRUE, 
+                  col.names = c("Name","Group"),
+                  encoding = "UTF-8")
+
+# Import edge list: contains edges$Source, edges$Target, edges$Weight, edges$Relation
+edges <- read.csv('new_Edges.csv',
+                  header = TRUE, 
+                  sep=",",
+                  col.names = c("Source","Target","Relation"),
+                  encoding = "UTF-8")
+
+# Remove leading and trailing whitespace
+nodes$Name <- stri_trim(nodes$Name)
+nodes$Group <- stri_trim(nodes$Group)
+edges$Source <- stri_trim(edges$Source)
+edges$Target <- stri_trim(edges$Target)
+edges$Relation <- stri_trim(edges$Relation)
+
+# Verifica consistencia vertices - ejes
+edges_ = c(edges$Source,edges$Target)
+length(edges_)
+missing = edges_ %in% nodes$Name
+missing_indx = which(missing == FALSE)
+edges_[missing_indx]
+table(edges_ %in% nodes$Name)
+
+
+# Define ----
+ui <- fluidPage(
+  
+  # title = textOutput(outputId = "title"),
+  # h3("Using igraph...",align="left"),
+  # p("Here you are with this amazing network..."),
+  
+  visNetworkOutput(outputId="network", height = 550),
+  
+  hr(),
+  
+  fluidRow(
+    column(3,offset = 1,
+           h4("Network Ties"),
+           br(),
+           # Selection of the edges that will appear in the relation network 
+           checkboxGroupInput("edges_select",
+                              label = "Tie Type:",
+                              choices = list("Is teacher of" = 1, "Is friend of" = 2, "Is family of" = 3),
+                              selected = 1,
+                              inline = FALSE)
+    ),
+    column(4,
+           # Egonet configuration
+           h4("Egos"),
+           br(),
+           # Node selection
+           uiOutput(outputId = "node_sel"),
+           # Order
+           sliderInput(inputId = "order", label = "Order", 1, 4, 2, step = 1)
+    ),
+    column(4,
+           # Node and Label Size
+           h4("Appearance"),
+           br(),
+           sliderInput(inputId = "label_size", label = "Label Size", min = 0.3, max = 1.0, value = c(0.4, 0.9),ticks = FALSE),
+           br(),
+           sliderInput(inputId = "node_size", label = "Node Size", min = 3.0, max = 30.0, value = c(8.0, 20.0),ticks = FALSE),
+           hr(),
+           helpText("These sliders set the range for node sizes and label sizes according to the degree of each node")
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+	 # function to rescale node degree
+	rescale <- function(x,a,b,c,d){c + (x-a)/(b-a)*(d-c)}
+	
+	output$network = renderVisNetwork({
+	  			 		  
+	  g = graph_from_data_frame(d = edges, directed=TRUE, vertices = nodes)
+
+		# id en nodos es necesario para exportar al formato Pajek
+		V(g)$id = V(g)$name
+		
+		# id en ejes es necesario para visUpdateNodes / visRemoveNodes / visUpdateEdges / visRemoveEdges 
+		E(g)$id = seq(1:length(E(g)))
+
+		g_ <- subgraph.edges(g,
+		                     which(E(g)$Relation == "is teacher of"))
+		#subTitle = "Type of ties: Teacher"
+		
+		# Egonet
+		d <- make_ego_graph(g_,
+		                    order = 2,
+		                    nodes = "Plato", 
+		                    mode = c("all"), 
+		                    mindist = 0)
+		
+		labsize <- rescale(degree(d[[1]]), min(degree(d[[1]])), max(degree(d[[1]])), 0.3, 1.0)
+		V(d[[1]])$label.cex <- labsize
+		
+		#subTitle = paste0("Egonet of Variable ", "Plato", ", ",subTitle)
+		
+		data <- toVisNetworkData(d[[1]])
+		
+		nodesize <- rescale(degree(d[[1]]), min(degree(d[[1]])), max(degree(d[[1]])), 3, 30)
+		data$nodes$size = nodesize
+
+		data$nodes$color.background = case_when(
+		  data$nodes$Group == "Male" ~ '#FF6347',
+		  data$nodes$Group == "Female" ~ '#ffa500'
+		)
+		
+		data$nodes$color.border = rep("#000000",length(data$nodes$color.background))
+		
+		data$nodes$color.highlight = case_when(
+		  data$nodes$Group == "Male" ~ '#47e3ff',
+		  data$nodes$Group == "Female" ~ '#005aff'
+		)
+		
+		visNetwork(nodes = data$nodes, edges = data$edges)%>%
+		  visNodes(shape = "dot")%>%
+		  visEdges(arrows =list(to = list(enabled = TRUE)),
+		           color = list(color = "gray",
+		                        highlight = "red"),
+		           smooth = TRUE) %>%  
+		  visOptions(highlightNearest = TRUE)%>%
+		  visPhysics(stabilization = list("fit" = TRUE))
+	})
+	
+
+	observe({
+	  
+	  if(is.null(input$edges_select)) stop("Please select at least 1 type of edge")
+
+	  g = graph_from_data_frame(d = edges, directed=TRUE, vertices = nodes)
+
+	  # Es necesario definir una columna de id para exportar correctamente al formato Pajek
+	  V(g)$id = V(g)$name
+	  # Id of edges required for visnetwork
+	  E(g)$id = seq(1:length(E(g)))
+
+	  if (length(input$edges_select) == 1){
+	    if (input$edges_select[1] == 1){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation == "is teacher of"))
+	      #subTitle = "Type of ties: Teacher"
+	    }
+	    if (input$edges_select[1] == 2){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is friend of"))
+	      #subTitle = "Type of ties: Friends"
+	    }
+	    if (input$edges_select[1] == 3){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is family of"))
+	      #subTitle = "Type of ties: Family"
+	    }
+	  }
+	  if (length(input$edges_select) == 2){
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 2){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is friend of"))
+	      #subTitle = "Type of ties: Teacher, Friend"
+	    }
+	    if (input$edges_select[1] == 2 & input$edges_select[2] == 3){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is friend of" | E(g)$Relation=="is family of"))
+	      #subTitle = "Type of ties: Friends, Family"
+	    }
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 3){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is family of"))
+	      #subTitle = "Type of ties: Teacher, Family"
+	    }
+	  }
+	  if (length(input$edges_select) == 3){
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 2 & input$edges_select[3] == 3){
+	      g_ <- subgraph.edges(g,
+	                           which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is friend of" | E(g)$Relation=="is family of"))
+	      #subTitle = "Type of ties: Teacher, Friends, Family"
+	    }
+	  }
+
+	  full_data <- toVisNetworkData(g)
+
+	  if (is.null(input$node_sel)) ego_node = "Plato"
+	  else ego_node = input$node_sel
+
+	  # Egonet
+	  d <- make_ego_graph(g_,
+	                      order = input$order,
+	                      nodes = ego_node,
+	                      mode = c("all"),
+	                      mindist = 0)
+
+	  #Set label size
+	  if (is.null(input$label_size[1])) min_label = 0.3
+	  else min_label = input$label_size[1]
+
+	  if (is.null(input$label_size[2])) max_label = 1.0
+	  else max_label = input$label_size[2]
+
+	  labsize <- rescale(degree(d[[1]]), min(degree(d[[1]])), max(degree(d[[1]])), min_label, max_label)
+	  V(d[[1]])$label.cex <- labsize
+
+	  #subTitle = paste0("Egonet of Variable ", ego_node, ", ",subTitle)
+
+	  data <- toVisNetworkData(d[[1]])
+
+	  # Set node size
+	  if (is.null(input$node_size[1])) min_node = 3.0
+	  else min_node = input$node_size[1]
+
+	  if (is.null(input$node_size[2])) max_node = 30.0
+	  else max_node = input$node_size[2]
+
+	  nodesize <- rescale(degree(d[[1]]), min(degree(d[[1]])), max(degree(d[[1]])), min_node, max_node)
+	  data$nodes$size = nodesize
+
+	  data$nodes$color.background = case_when(
+	    data$nodes$Group == "Male" ~ '#FF6347',
+	    data$nodes$Group == "Female" ~ '#ffa500'
+	  )
+
+	  data$nodes$color.border = rep("#000000",length(data$nodes$color.background))
+
+	  data$nodes$color.highlight = case_when(
+	    data$nodes$Group == "Male" ~ '#47e3ff',
+	    data$nodes$Group == "Female" ~ '#005aff'
+	  )
+
+	  nodes_to_remove = full_data$nodes[!full_data$nodes %in% data$nodes]
+	  edges_to_remove = full_data$edges[!full_data$edges %in% data$edges]
+
+	  visNetworkProxy("network") %>%
+	    #visSetTitle(main = subTitle) %>%
+	    visRemoveNodes(id = nodes_to_remove$id) %>%
+	    visRemoveEdges(id = edges_to_remove$id) %>%
+	    visUpdateNodes(nodes = data$nodes) %>%
+	    visUpdateEdges(edges = data$edges) %>%
+	    visPhysics(stabilization = list("fit" = TRUE)) %>%
+	    visEdges(arrows = list(to = list(enabled = FALSE)),
+	             color = list(color = "gray",
+	             highlight = "red"),
+	             smooth = TRUE)
+	    
+	  if(length(input$edges_select) == 1 & input$edges_select[1] == 1) {
+	    visNetworkProxy("network") %>%
+	      visEdges(arrows =list(to = list(enabled = TRUE)), smooth =T)
+	  }
+	  
+	})
+	
+	output$node_sel <- renderUI({
+	  
+	  if (length(input$edges_select) == 1){
+	    if (input$edges_select[1] == 1){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is teacher of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is teacher of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }
+	    if (input$edges_select[1] == 2){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is friend of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is friend of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }  
+	    if (input$edges_select[1] == 3){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is family of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is family of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }
+	  }
+	  if (length(input$edges_select) == 2){
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 2){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is friend of" | edges$Relation == "is teacher of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is friend of" | edges$Relation == "is teacher of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }  
+	    if (input$edges_select[1] == 2 & input$edges_select[2] == 3){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is friend of" | edges$Relation == "is family of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is friend of" | edges$Relation == "is family of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 3){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is family of" | edges$Relation == "is teacher of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is family of" | edges$Relation == "is teacher of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }  
+	  }
+	  if (length(input$edges_select) == 3){
+	    if (input$edges_select[1] == 1 & input$edges_select[2] == 2 & input$edges_select[3] == 3){
+	      all_nodes = c(as.character(edges$Source[which(edges$Relation == "is friend of" | edges$Relation == "is family of" | edges$Relation == "is teacher of")]),
+	                    as.character(edges$Target[which(edges$Relation == "is friend of" | edges$Relation == "is family of" | edges$Relation == "is teacher of")])
+	      )
+	      all_nodes = unique(all_nodes)
+	    }  
+	  }
+	  selectInput(inputId = "node_sel", label = "Node", choices = all_nodes, selected = "Plato")
+	})
+}
+
+shinyApp(ui = ui, server = server)
---
+++ network_vnw/app.R
@@ -0,0 +1,214 @@
+
+library(shiny)
+library(rmarkdown)
+library(knitr)
+library(rsconnect)
+library(igraph)
+library(stringi)
+library(tidyverse)
+library(visNetwork)
+
+# Network visnetwork
+
+# Import node list: contains nodes$Name, nodes$Group
+nodes <- read.csv('new_Nodes.csv', 
+                  sep=",", 
+                  header = TRUE, 
+                  col.names = c("Name","Group"),
+                  encoding = "UTF-8")
+
+# Import edge list: contains edges$Source, edges$Target, edges$Weight, edges$Relation
+edges <- read.csv('new_Edges.csv',
+                  header = TRUE, 
+                  sep=",",
+                  col.names = c("Source","Target","Relation"),
+                  encoding = "UTF-8")
+
+# Remove leading and trailing whitespace
+nodes$Name <- stri_trim(nodes$Name)
+nodes$Group <- stri_trim(nodes$Group)
+edges$Source <- stri_trim(edges$Source)
+edges$Target <- stri_trim(edges$Target)
+edges$Relation <- stri_trim(edges$Relation)
+
+# Verifica consistencia vertices - ejes
+edges_ = c(edges$Source,edges$Target)
+length(edges_)
+missing = edges_ %in% nodes$Name
+missing_indx = which(missing == FALSE)
+edges_[missing_indx]
+table(edges_ %in% nodes$Name)
+
+# Define ----
+ui <- fluidPage(
+  
+  visNetworkOutput(outputId="network", height = 550),
+  
+  hr(),
+  
+  fluidRow(
+    column(1,offset=1
+           
+    ),
+    column(3, offset=1,
+           h4("Network Ties"),
+           br(),
+           # Selection of the edges that will appear in the relation network 
+           checkboxGroupInput("edges_select",
+                              label = "Tie Type",
+                              choices = list("Is teacher of" = 1, "Is friend of" = 2, "Is family of" = 3),
+                              selected = 1,
+                              inline = FALSE)
+    ),
+    column(6,
+           # Plot Height
+           h4("Appearence"),
+           br(),
+           sliderInput(inputId = "label_size", label = "Label Size", min = 0.2, max = 2.0, value = c(0.4, 1.6),ticks = FALSE)
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+	 # function to rescale node degree
+	rescale <- function(x,a,b,c,d){c + (x-a)/(b-a)*(d-c)}
+
+	output$network = renderVisNetwork({
+
+	  g = graph_from_data_frame(d = edges, directed=TRUE, vertices = nodes)
+		
+		# id en nodos es necesario para exportar al formato Pajek
+		V(g)$id = V(g)$name
+		
+		# id en ejes es necesario para visUpdateNodes / visRemoveNodes / visUpdateEdges / visRemoveEdges 
+		E(g)$id = seq(1:length(E(g)))
+
+		g_ <- subgraph.edges(g,
+						   which(E(g)$Relation == "is teacher of"))
+		subTitle = "Ties: teachers and students"
+		
+		labsize <- rescale(degree(g_), min(degree(g_)), max(degree(g_)), 0.2, 2.0)
+		V(g_)$label.cex <- labsize
+		
+		data <- toVisNetworkData(g_)
+		
+		data$nodes$color.background = case_when(
+		  data$nodes$Group == "Male" ~ '#FF6347',
+		  data$nodes$Group == "Female" ~ '#ffa500'
+		  )
+		
+		data$nodes$color.border = rep("#000000",length(data$nodes$color.background))
+		
+		data$nodes$color.highlight = case_when(
+		  data$nodes$Group == "Male" ~ '#47e3ff',
+		  data$nodes$Group == "Female" ~ '#005aff'
+		)
+		
+		visNetwork(nodes = data$nodes, edges = data$edges, main = "Ties: teachers")%>%
+		  visNodes(shape = "circle") %>%
+		  visEdges(arrows =list(to = list(enabled = TRUE)),
+		           color = list(color = "gray",
+		                        highlight = "red")) %>%
+		  visLegend(enabled = TRUE)%>%
+		  visOptions(highlightNearest = TRUE)%>%
+		  visPhysics(stabilization = TRUE)
+  })
+  	
+	
+	observe({
+	  
+  	if(is.null(input$edges_select)) stop("Please select at least 1 type of edge")
+	  
+	  g = graph_from_data_frame(d = edges, directed=TRUE, vertices = nodes)
+
+  	# id of vertex required to export properly to pajek
+  	V(g)$id = V(g)$name
+  	# Id of edges required for visnetwork
+  	E(g)$id = seq(1:length(E(g)))
+
+  	if (length(input$edges_select) == 1){
+  	  if (input$edges_select[1] == 1){
+  		g_ <- subgraph.edges(g,
+  						   which(E(g)$Relation == "is teacher of"))
+  		subTitle = "Ties: teacher-student "
+  	  }
+  	  if (input$edges_select[1] == 2){
+  		g_ <- subgraph.edges(g,
+  							 which(E(g)$Relation=="is friend of"))
+  		subTitle = "Ties: friends"
+  	  }
+  	  if (input$edges_select[1] == 3){
+  	  g_ <- subgraph.edges(g,
+  						   which(E(g)$Relation=="is family of"))
+  	  subTitle = "ties family"
+  	  }
+  	}
+  	if (length(input$edges_select) == 2){
+  	  if (input$edges_select[1] == 1 & input$edges_select[2] == 2){
+  		g_ <- subgraph.edges(g,
+  						   which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is friend of"))
+  		subTitle = "Ties: teachers and friends"
+  	  }
+  	  if (input$edges_select[1] == 2 & input$edges_select[2] == 3){
+  		g_ <- subgraph.edges(g,
+  							 which(E(g)$Relation=="is friend of" | E(g)$Relation=="is family of"))
+  		subTitle = "Ties: friends and family"
+  	  }
+  	  if (input$edges_select[1] == 1 & input$edges_select[2] == 3){
+  		g_ <- subgraph.edges(g,
+  							 which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is family of"))
+  		subTitle = "Ties: teachers and Family"
+  	  }
+  	}
+  	if (length(input$edges_select) == 3){
+  	  if (input$edges_select[1] == 1 & input$edges_select[2] == 2 & input$edges_select[3] == 3){
+  		g_ <- subgraph.edges(g,
+  							 which(E(g)$Relation=="is teacher of" | E(g)$Relation=="is friend of" | E(g)$Relation=="is family of"))
+  		subTitle = "Ties: teachers, friends, and family"
+  	  }
+  	}
+  	
+  	# Scale node label size (min deg., max deg., min size, max size)
+  	labsize <- rescale(degree(g_), min(degree(g_)), max(degree(g_)), input$label_size[1], input$label_size[2])
+  	V(g_)$label.cex <- labsize
+  	
+  	full_data <- toVisNetworkData(g)
+  	data <- toVisNetworkData(g_)
+  	
+  	data$nodes$color.background = case_when(
+  	  data$nodes$Group == "Male" ~ '#FF6347',
+  	  data$nodes$Group == "Female" ~ '#ffa500'
+  	)
+  	
+  	data$nodes$color.border = rep("#000000",length(data$nodes$color.background))
+  	
+  	data$nodes$color.highlight = case_when(
+  	  data$nodes$Group == "Male" ~ '#47e3ff',
+  	  data$nodes$Group == "Female" ~ '#005aff'
+  	)
+  	
+  	nodes_to_remove = full_data$nodes[!full_data$nodes %in% data$nodes]
+  	edges_to_remove = full_data$edges[!full_data$edges %in% data$edges]
+  	
+  	visNetworkProxy("network") %>%
+  	  visSetTitle(main = subTitle) %>%
+  	  visRemoveNodes(id = nodes_to_remove$id) %>%
+  	  visRemoveEdges(id = edges_to_remove$id) %>%
+  	  visUpdateNodes(nodes = data$nodes) %>%
+  	  visUpdateEdges(edges = data$edges) %>%
+  	  visEdges(arrows =list(to = list(enabled = FALSE)),
+  	           color = list(color = "gray",
+  	                        highlight = "red"))%>%
+  	  visPhysics(stabilization = TRUE)
+  	
+  	if(length(input$edges_select) == 1 & input$edges_select[1] == 1) {
+  	  visNetworkProxy("network") %>%
+  	  visEdges(arrows =list(to = list(enabled = TRUE)), smooth =T)
+  	}
+  	
+	})
+}
+
+shinyApp(ui = ui, server = server)
