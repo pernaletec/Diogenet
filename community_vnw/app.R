cb5d8a7449cf080fc28cbde6d6d4e19b01cfecdb
pernaletec <pernalete.cg@gmail.com>
2019-02-17 18:46:24 -0400

    Enable hovering functions and legend

--- community_vnw/app.R
+++ community_vnw/app.R
@@ -73,12 +73,12 @@ ui <- fluidPage(
            # Plot Height
            h4("Appearance"),
            br(),
-           # Plot Size
-           sliderInput(inputId = "plot_height", label = "Plot Height (px)", 500, 1200, 600, step = 30,ticks = FALSE,post = "px"),
            # Label Size
            sliderInput(inputId = "label_size", label = "Label Size", min = 0.0, max = 5.0, value = c(1, 4),ticks = FALSE),
            # Node Size
-           sliderInput(inputId = "node_size", label = "Node Size", min = 10.0, max = 60.0, value = c(20, 40),ticks = FALSE)
+           sliderInput(inputId = "node_size", label = "Node Size", min = 10.0, max = 60.0, value = c(20, 40),ticks = FALSE),
+           hr(),
+           helpText("These parameters control de size range of nodes and labels. The minimum size is set to the nodes with the lowest degree, while the maximum size is set to nodes with the highest degree. The same applies for its labels")
     )
   )
 )
@@ -89,9 +89,6 @@ server <- function(input, output) {
   # function to rescale node degree
   rescale <- function(x,a,b,c,d){c + (x-a)/(b-a)*(d-c)}
 	
-	 # function to rescale node degree
-	rescale <- function(x,a,b,c,d){c + (x-a)/(b-a)*(d-c)}
-	
   output$network = renderVisNetwork({
 
   	    # First validate if there are edges (Ties) selected
@@ -100,7 +97,6 @@ server <- function(input, output) {
   	    )
 
   	    g = graph_from_data_frame(d = edges, directed=FALSE, vertices = nodes)
-
 
   	    # ids in nodes are es required to export to Pajek format. Just in case!
         V(g)$id = V(g)$name
@@ -181,21 +177,15 @@ server <- function(input, output) {
           data$edges$Relation == "is teacher of" ~ '#0000FF',
           data$edges$Relation == "is friend of" ~ '#228B22',
           data$edges$Relation == "is family of" ~ '#FF0000',
-          data$edges$Relation == "studied the work of" ~ '#FF0000'
+          data$edges$Relation == "studied the work of" ~ '#ff8c00'
         )
         
         data$edges$color.highlight = case_when(
           data$edges$Relation == "is teacher of" ~ '#00ffff',
           data$edges$Relation == "is friend of" ~ '#568b22',
-          data$edges$Relation == "is family of" ~ '#ff4000',
-          data$edges$Relation == "studied the work of" ~ '#ff4000'
+          data$edges$Relation == "is family of" ~ '#00ff00',
+          data$edges$Relation == "studied the work of" ~ '#1a00ff'
         )
-        
-        # Shows the name when hovering over the node
-        data$nodes$title = paste0("<b>",data$nodes$id,"</b>","<br/>" , "Community N°: ", "<b>",data$nodes$community,"</b>")
-        
-        # Shows the relation when hovering over the edge
-        data$edges$title = paste0("<i>",data$edges$Relation,"</i>")
         
         # # nodes data.frame for legend
         # lnodes <- data.frame(label = c("Male", "Female"),
@@ -203,11 +193,18 @@ server <- function(input, output) {
         #                      color = c("#FF6347", "#ffa500"),
         #                      id = 1:2)
         # 
-        # # edges data.frame for legend
-        # ledges <- data.frame(color = c("#0000FF", "#228B22", "#FF0000"),
-        #                      label = c("is teacher of", "is friend of", "is family of"), 
-        #                      arrows =c("to", FALSE, FALSE), 
-        #                      font.align = "bottom")
+        
+        # edges data.frame for legend
+        ledges <- data.frame(color = c("#0000FF", "#228B22", "#FF0000", "#ff8c00"),
+                             label = c("is teacher of", "is friend of", "is family of", "studied the work of"),
+                             arrows =c("to", FALSE, FALSE, FALSE),
+                             font.align = "bottom")
+        
+        # Shows the name when hovering over the node
+        data$nodes$title = paste0("<b>",data$nodes$id,"</b>","<br/>" , "Community N°: ", "<b>",data$nodes$community,"</b>")
+        
+        # Shows the relation when hovering over the edge
+        data$edges$title = paste0("<i>",data$edges$Relation,"</i>")
       
         withProgress(message = 'Creating graph', style = 'notification', value = 0.1, {
           Sys.sleep(0.25)
@@ -216,10 +213,10 @@ server <- function(input, output) {
       		subTitle = paste0("MODULARITY: ", mod)
       		
       		# Visnetwork graph creation
-      		visNetwork(nodes = data$nodes, edges = data$edges)%>%
+      		visNetwork(nodes = data$nodes, edges = data$edges, main=subTitle)%>%
       		  visNodes(shape = "dot") %>%
       		  visEdges(arrows =list(to = list(enabled = FALSE))) %>%
-      		  #visLegend(addEdges = ledges, addNodes = lnodes, useGroups = FALSE, width = 0.15, zoom = FALSE)%>%
+      		  visLegend(addEdges = ledges, addNodes = lnodes, useGroups = FALSE, width = 0.15, zoom = FALSE)%>%
       		  visIgraphLayout()%>%
       		  visOptions(highlightNearest = TRUE)
       		
